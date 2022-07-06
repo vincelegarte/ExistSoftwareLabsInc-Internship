fc55b0a4b88d2c15a883aaf0ce0c9e9563ae364d
vincelegarte <vince.legarte@gmail.com>
2022-07-06 16:03:30 +0800

    created relationship between employee and ticket

--- Activity4/src/main/java/com/activity/four/employee/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.activity.four.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
